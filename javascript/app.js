1759e8571dbe091f0b4e551dc523d32d57fc80e5
abdulmajjed <abddul.majjed.najjar@gmail.com>
2021-03-20 17:48:52 +0200

    JS

---
+++ javascript/app.js
@@ -0,0 +1,143 @@
+// DataTypes
+
+/*
+- string: Text in JS
+
+-numbers: All types of numbers in JS
+
+-Boolean: True or False
+
+
+
+// "Hello my name is abdulmajjed"
+// 'strings' 
+
+// alert(5+5);
+
+
+
+var x=5;
+var y=7;
+ 
+alert (x+y);
+// operators: sympol that does an action
+// asignment operator : 
+// =
+// += 
+x+=7;
+//
+x=x+7
+//-=
+//x-=10
+//x=x-10
+
+// arithmatic operators: 
+// + plus
+// - minus
+// * multypliy
+// / devide
+// % modules
+// ** power of\
+var hungery = True;
+*/ 
+
+
+/*
+
+alert(UserName);
+var x=5;
+var y=7;
+console.log(x+y);
+*/
+var UserName=" Abdulmajjed";
+var FirstName="Adulmajjed"
+var LastName="Al-Najjar"
+var FullName=FirstName+LastName
+// concatination
+
+
+console.log(FullName);
+
+
+var UserName= prompt("What's your name");
+console.log(UserName);
+
+document.write('<h3>'+UserName+'</h3>');
+alert('choose between Classical Or Family one!')
+/*
+== compare the value only
+=== compare the value and Datattyoe
+!= not equal value only
+!== not equal value and datatypes
+< smaller than 
+>  biggerthan
+>= smaller than or equal
+<=  bigger tha or equal
+
+
+var FirstName="Adulmajjed"
+var LastName="Al-Najjar"
+
+ //   var age=15;
+//    var age2=20;
+
+ //   console.log(age>age2);
+ if(conditions){ 
+    if true do this code... 
+}
+
+
+var hungrey=true;
+var tired=true;
+
+if(hungrey==true)
+{console.log('im not working');
+}else if(tired==true)
+{
+    console.log('not working cause tired');
+}
+
+else{
+    console.log("im working")
+}
+
+
+
+
+1- Ask the user whats your favorite artist
+
+2- show image based on answer 
+
+
+Var CampingType=prompt("How to know the teste ?");
+
+
+if(CampingType=='Classical'){
+    document.write('<img src="');  
+ else if {(CampingType==Family)
+    document.write( );
+}
+else{
+
+    alert( "choose Classical ,Family or Descovering "
+    
+*/
+
+var CampingType=prompt("its Classical or Family?" );
+
+   
+  if
+  (CampingType==='classical'){
+      document.write('<img src="pictures/classical.jpg"/>');
+  }else if (CampingType==='Family'){
+          document.write('<img src="pictures/family.jpg"/>');
+      }else{
+      alert("have fun !!")
+     }     
+      
+  
+
+
+console.log('CampingType')
+
+
